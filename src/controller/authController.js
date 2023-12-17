d072ad28209402c92da4b13b2f2456b4d1055aac
AgungBahtiarr <agungbahtiar050@gmail.com>
2023-12-17 19:49:04 +0700

    fix: events schema

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -8,29 +8,37 @@ const register = async (req, res) => {
   const error = validationResult(req);
 
   if (!error.isEmpty()) {
-    res.status(400).json({
+    return res.status(400).json({
       status: false,
       error,
       data: null,
     });
   } else {
-    const { username, email, roleId, password } = req.body;
+    try {
+      const { username, email, roleId, password } = req.body;
 
-    const hashPassword = bcrypt.hashSync(password, 10);
+      const hashPassword = bcrypt.hashSync(password, 10);
 
-    const user = await prisma.users.create({
-      data: {
-        username,
-        email,
-        password: hashPassword,
-        roleId: parseInt(roleId),
-      },
-    });
-    res.status(201).json({
-      status: true,
-      error: null,
-      data: user,
-    });
+      const user = await prisma.users.create({
+        data: {
+          username,
+          email,
+          password: hashPassword,
+          roleId: parseInt(roleId),
+        },
+      });
+      return res.status(201).json({
+        status: true,
+        error: null,
+        data: user,
+      });
+    } catch (e) {
+      return res.status(400).json({
+        status: false,
+        error: e,
+        data: null,
+      });
+    }
   }
 };
 
@@ -38,50 +46,60 @@ const login = async (req, res) => {
   const error = validationResult(req);
 
   if (!error.isEmpty()) {
-    res.status(400).json({
+    return res.status(400).json({
       status: false,
       error,
-    });
-  }
-  const { email, password } = req.body;
-
-  const user = await prisma.users.findUnique({
-    where: {
-      email: email,
-    },
-  });
-
-  if (!user) {
-    res.status(400).json({
-      status: false,
-      error: "user not found",
       data: null,
     });
-  } else {
-    const isValid = bcrypt.compareSync(password, user.password);
-    if (!isValid) {
-      res.status(400).json({
+  }
+
+  try {
+    const { email, password } = req.body;
+
+    const user = await prisma.users.findUnique({
+      where: {
+        email: email,
+      },
+    });
+
+    if (!user) {
+      return res.status(400).json({
         status: false,
-        error: "Password Invalid",
+        error: "user not found",
         data: null,
       });
     } else {
-      const payload = {
-        id: user.id,
-        username: user.username,
-        email: user.email,
-        roleId: user.roleId,
-      };
-      const secret = process.env.JWT_SECRET;
-      const exp = 60 * 60 * 12;
-      const token = jwt.sign(payload, secret, { expiresIn: exp });
-      res.status(200).json({
-        status: true,
-        error: null,
-        data: user,
-        token: token,
-      });
+      const isValid = bcrypt.compareSync(password, user.password);
+      if (!isValid) {
+        return res.status(400).json({
+          status: false,
+          error: "Password Invalid",
+          data: null,
+        });
+      } else {
+        const payload = {
+          id: user.id,
+          username: user.username,
+          email: user.email,
+          roleId: user.roleId,
+        };
+        const secret = process.env.JWT_SECRET;
+        const exp = 60 * 60 * 12;
+        const token = jwt.sign(payload, secret, { expiresIn: exp });
+        return res.status(200).json({
+          status: true,
+          error: null,
+          data: user,
+          token: token,
+        });
+      }
     }
+  } catch (e) {
+    return res.status(400).json({
+      status: false,
+      error: e,
+      data: null,
+    });
   }
 };
 
@@ -89,13 +107,13 @@ const logout = async (req, res) => {
   req.userData = null;
 
   if (!req.userData) {
-    res.status(200).json({
+    return res.status(200).json({
       status: true,
       error: null,
       data: "You are logged out",
     });
   } else {
-    res.statu(400).json({
+    return res.status(400).json({
       status: false,
       error: null,
       data: "Logout failed",
